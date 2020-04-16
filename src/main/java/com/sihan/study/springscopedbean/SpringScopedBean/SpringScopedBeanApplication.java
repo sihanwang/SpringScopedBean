3aa81e0883f43978e3b8ac7053c5128682de2b54
sihanwang <ocpwj@hotmail.com>
2020-04-16 13:36:02 +0800

    init

---
+++ src/main/java/com/sihan/study/springscopedbean/SpringScopedBean/SpringScopedBeanApplication.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springscopedbean.SpringScopedBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScopedBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScopedBeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/springscopedbean/SpringScopedBean/SpringScopedBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springscopedbean.SpringScopedBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScopedBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
